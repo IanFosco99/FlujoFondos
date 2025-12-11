915b3d139f8d27adf825010a3aedf8395797c845
JMonserrat1 <monserratjere@gmail.com>
2025-12-11 19:43:47 -0300

    push de la abse de datos de mario

--- src/main/java/Archivos/flujo_fondos.sql
+++ src/main/java/Archivos/flujo_fondos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-11-2025 a las 00:19:23
+-- Tiempo de generación: 11-12-2025 a las 22:26:44
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -45,9 +45,12 @@ CREATE TABLE `cheque_propio` (
 --
 
 INSERT INTO `cheque_propio` (`id_cheque`, `nro_cheque`, `importe_cheque`, `fecha_cobro_cheque`, `estado_cheque`, `observacion_cheque`, `titular_destino`, `uso_cheque`, `id_cuenta_salida`, `fecha_entrega_cheque`) VALUES
-(1, 123123, 120000.00, '2025-10-22', 0, '', 0, '', 0, NULL),
-(2, 1234, 28000000.00, '2025-10-30', 0, '', 0, '', 0, NULL),
-(4, 12, 2131233.00, '2025-11-13', 0, 'sdasda', 2, NULL, 1, NULL);
+(1, 123123, 120000.00, '2025-10-22', 1, '', 1, '', 1, '2025-11-22'),
+(2, 1234, 2800.00, '2025-10-31', 1, 'venta contado', 2, '', 1, '2025-11-22'),
+(5, 122, 2000.00, '2025-11-16', 1, '', 3, NULL, 1, '2025-11-28'),
+(6, 22, 25000.00, '2025-11-21', 1, 'venta', 3, '', 1, '2025-11-28'),
+(7, 150600, 870000.00, '2025-12-11', 0, 'Pago de Materiales', 1, NULL, 3, NULL),
+(11, 500600, 3500000.00, '2025-12-11', 1, 'Pago de Sueldos', 2, NULL, 4, '2025-12-10');
 
 -- --------------------------------------------------------
 
@@ -75,10 +78,8 @@ CREATE TABLE `cheque_tercero` (
 --
 
 INSERT INTO `cheque_tercero` (`id_cheque`, `nro_cheque`, `importe_cheque`, `fecha_cobro_cheque`, `estado_cheque`, `observacion_cheque`, `fecha_entrega_cheque`, `titular_cheque`, `titular_destino`, `uso_cheque`, `id_cuenta_entrada`, `id_cuenta_salida`) VALUES
-(1, 23, 456.00, '2025-11-22', 0, 'ghb', NULL, 1, NULL, NULL, 1, NULL),
-(3, 22, 12000000.00, '2025-11-13', 0, 'ads', NULL, 3, NULL, NULL, 1, NULL),
-(8, 4, 15000000.00, '2025-11-14', 0, 'venta', NULL, 7, NULL, NULL, 1, NULL),
-(9, 6, 1233333.00, '2025-11-14', 0, 'compra', NULL, 3, NULL, NULL, 1, NULL);
+(3, 2520, 289000.00, '2025-12-15', 0, 'Cobro Cliente Perez Juan', NULL, 1, NULL, NULL, 2, NULL),
+(4, 3558, 750000.00, '2025-12-17', 0, 'Cobro Deuda 2025', '2025-12-10', 3, NULL, NULL, 2, NULL);
 
 -- --------------------------------------------------------
 
@@ -100,11 +101,10 @@ CREATE TABLE `cliente_proveedores` (
 --
 
 INSERT INTO `cliente_proveedores` (`id_cliente_proveedor`, `nom_razon_social`, `dni_cuit`, `telefono`, `email`, `tipo_cliente_proveedor`) VALUES
-(1, 'asdasdasd', 1234565432, '123132133', 'momn@gmail.com', 'Proveedor'),
-(2, 'asdasda', 123123213, '2131231231', 'sadad@sada.com', 'Cliente'),
+(1, 'Juan Perez', 1234565432, '123132133', 'momn@gmail.com', 'Proveedor'),
+(2, 'Jose Rodriguez', 123123213, '2131231231', 'sadad@sada.com', 'Cliente'),
 (3, 'HoodS Burguer', 46042631, '03476600093', 'Augusto11805@gmail.com', 'Cliente'),
-(4, 'SolNails', 44178222, '3476355560', 'solramirez835@gmail.com', 'Proveedor'),
-(7, 'Jere Monserrat', 45343327, '3476206695', 'monserratjere@gmail.com', 'Proveedor');
+(4, 'Sol Ramirez', 44178222, '3476355560', 'solramirez835@gmail.com', 'Proveedor');
 
 -- --------------------------------------------------------
 
@@ -118,7 +118,7 @@ CREATE TABLE `cuentas` (
   `nom_concepto` varchar(100) DEFAULT NULL,
   `clas_concepto` varchar(50) DEFAULT NULL,
   `id_movimiento` bigint(20) DEFAULT NULL,
-  `ingreso` int(1) DEFAULT NULL
+  `ingreso` varchar(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
@@ -126,8 +126,12 @@ CREATE TABLE `cuentas` (
 --
 
 INSERT INTO `cuentas` (`id_cuenta`, `cod_concepto`, `nom_concepto`, `clas_concepto`, `id_movimiento`, `ingreso`) VALUES
-(1, '1.01', 'Venta Contado', 'Operativo', 1, 0),
-(2, '1.23', 'aaaaa', 'Operativo', 1, 1);
+(1, '1.01', 'Venta de Contado', 'Operativo', 1, '1'),
+(2, '1.02', 'Ventas en Cta.Cte.', 'Operativo', 2, '1'),
+(3, '1.03', 'Proveedores', 'Operativo', 3, '0'),
+(4, '1.04', 'Sueldos y Cs. Sociales', 'Operativo', 2, '0'),
+(6, '2.01', 'Inversion en Bienes de Uso', 'Inversion', 4, '0'),
+(7, '3.01', 'Prestamos Recibidos', 'Financiacion', 1, '1');
 
 -- --------------------------------------------------------
 
@@ -143,7 +147,7 @@ CREATE TABLE `flujos_mov` (
   `importe` decimal(16,2) NOT NULL DEFAULT 0.00,
   `observaciones_mov` varchar(200) NOT NULL DEFAULT '0',
   `id_cheque` bigint(20) NOT NULL DEFAULT 0,
-  `id_cheque_tercero` bigint(20) NOT NULL DEFAULT 0
+  `id_cheque_tercero` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
@@ -151,7 +155,11 @@ CREATE TABLE `flujos_mov` (
 --
 
 INSERT INTO `flujos_mov` (`id_flujo_mov`, `fecha_mov`, `id_movimiento`, `id_cuenta`, `importe`, `observaciones_mov`, `id_cheque`, `id_cheque_tercero`) VALUES
-(1, '2025-10-16', 1, 1, 400000.00, '0', 0, 0);
+(1, '2025-12-10', 1, 1, 158000.00, 'Venta de Mercaderias', 0, 0),
+(2, '2025-12-10', 2, 2, 250000.00, 'Mercaderia en CtaCte', 0, 0),
+(4, '2025-12-10', 1, 7, 500000.00, 'MUFC', 0, 0),
+(5, '2025-12-11', 4, 3, -356000.00, 'Impuestos diciembre', 0, 0),
+(6, '2025-12-10', 4, 3, 875000.00, 'Compra AA', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -170,9 +178,9 @@ CREATE TABLE `movimiento` (
 
 INSERT INTO `movimiento` (`id_movimiento`, `desc_movimiento`) VALUES
 (1, 'Caja'),
-(2, 'Cheques a Cobrar'),
-(3, 'Cheques Emitidos'),
-(4, 'Banco El Tornillo Cta.Cte.');
+(2, 'Banco El Tornillo Cta Cte'),
+(3, 'Cheques a Cobrar'),
+(4, 'Cheques Emitidos');
 
 --
 -- Índices para tablas volcadas
@@ -210,7 +218,8 @@ ALTER TABLE `flujos_mov`
   ADD PRIMARY KEY (`id_flujo_mov`),
   ADD KEY `id_movimiento` (`id_movimiento`),
   ADD KEY `id_cuenta` (`id_cuenta`),
-  ADD KEY `id_cheque` (`id_cheque`);
+  ADD KEY `id_cheque` (`id_cheque`),
+  ADD KEY `id_cheque_tercero` (`id_cheque_tercero`);
 
 --
 -- Indices de la tabla `movimiento`
@@ -226,13 +235,13 @@ ALTER TABLE `movimiento`
 -- AUTO_INCREMENT de la tabla `cheque_propio`
 --
 ALTER TABLE `cheque_propio`
-  MODIFY `id_cheque` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_cheque` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `cheque_tercero`
 --
 ALTER TABLE `cheque_tercero`
-  MODIFY `id_cheque` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_cheque` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `cliente_proveedores`
@@ -244,19 +253,19 @@ ALTER TABLE `cliente_proveedores`
 -- AUTO_INCREMENT de la tabla `cuentas`
 --
 ALTER TABLE `cuentas`
-  MODIFY `id_cuenta` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_cuenta` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `flujos_mov`
 --
 ALTER TABLE `flujos_mov`
-  MODIFY `id_flujo_mov` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_flujo_mov` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `movimiento`
 --
 ALTER TABLE `movimiento`
-  MODIFY `id_movimiento` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_movimiento` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
